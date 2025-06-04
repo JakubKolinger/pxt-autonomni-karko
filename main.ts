0f23907bfa667c5f0ef704fd3615c4bfe4d68551
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-06-04 12:37:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,13 +14,32 @@ pins.setPull(IR.p, PinPullMode.PullNone);
 
 let L: number;
 let P: number;
-let data: number;
+let dataL: number;
+let dataC: number;
+let dataP: number;
 
 basic.forever(function () {
-    data = pins.digitalReadPin(IR.l);
-    basic.showNumber(data)
+    dataL = pins.digitalReadPin(IR.l);
+    dataC = pins.digitalReadPin(IR.c);
+    dataP = pins.digitalReadPin(IR.p);
+    
+})
+basic.forever(function () {
+    if (dataC === 1 && dataL === 0 && dataP === 0){
+        L = 250
+        P = 250
+    }
+    if (dataC === 0 && dataP === 0 && dataL === 1) {
+        P = 150
+        L = 50
+    }
+    if (dataC === 0 && dataL === 0 && dataP === 1) {
+        P = 50
+        L = 150
+    }
 })
 
-
-
-Sensors.ping(DigitalPin.P0, DigitalPin.P0, null)
+basic.forever(function () {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, L)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, P)
+})
