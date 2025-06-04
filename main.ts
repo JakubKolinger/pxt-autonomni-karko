23f69eb65915455983876592a456e87f849fa20d
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-06-04 12:18:50 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
 type IRC = {
     l: DigitalPin,
     c: DigitalPin,
-    r: DigitalPin
+    p: DigitalPin
 }
 const IR: IRC = {
     l: DigitalPin.P14,
     c: DigitalPin.P15,
-    r: DigitalPin.P13
+    p: DigitalPin.P13
 }
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
-pins.setPull(IR.r, PinPullMode.PullNone);
+pins.setPull(IR.p, PinPullMode.PullNone);
+
+let L: number;
+let P: number;
+let data: number;
+
+basic.forever(function () {
+    data = pins.digitalReadPin(IR.l);
+})
+
+
+
+
+Sensors.ping(DigitalPin.P0, DigitalPin.P0, null)
