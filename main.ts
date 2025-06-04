b477bd5d29f67307971b1d2f706698ebd230b5f8
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-06-04 11:58:45 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+type IRC = {
+    l: DigitalPin,
+    c: DigitalPin,
+    r: DigitalPin
+}
+const IR: IRC = {
+    l: DigitalPin.P14,
+    c: DigitalPin.P15,
+    r: DigitalPin.P13
+}
+pins.setPull(IR.l, PinPullMode.PullNone);
+pins.setPull(IR.c, PinPullMode.PullNone);
+pins.setPull(IR.r, PinPullMode.PullNone);
