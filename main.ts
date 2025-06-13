2c16a97eb398e54e65833c7ea51b41f9181ea18d
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-06-13 13:01:03 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -23,6 +23,7 @@ let P: number;
 let dataL: number;
 let dataC: number;
 let dataP: number;
+let krizovatka = false;
 
 basic.forever(function () {
     dataL = pins.digitalReadPin(IR.l);
@@ -34,39 +35,49 @@ basic.forever(function () {
 radio.onReceivedNumber(function(receivedNumber: number) {
     let serial: number = radio.receivedPacket(RadioPacketProperty.SerialNumber)
     if (serial = -978678300) {
-        if (receivedNumber === 1) {
-            P = 100
-            L = 50
+        if (krizovatka === true) {
+            if (receivedNumber === 1) {
+                P = 250
+                L = 50
+                krizovatka = false
+            }
+            if (receivedNumber === 2) {
+                L = 250
+                P = 50
+                krizovatka = false
+            }
+            if (receivedNumber === 3) {
+                P = 150
+                L = 150
+                krizovatka = false
+            }
         }
-        if (receivedNumber === 2) {
-            L = 100
-            P = 50
-        }
-        if (receivedNumber === 3) {
-            P = 100
-            L = 100
-        }
+        
     }
     
 
 })
 basic.forever(function () {
-    if (dataC === 1 && dataL === 0 && dataP === 0){
-        L = 150
-        P = 150
+    if (krizovatka === false) {
+        if (dataC === 1 && dataL === 0 && dataP === 0) {
+            L = 150
+            P = 150
+        }
+        if (dataP === 0 && dataL === 1) {
+            P = 250
+            L = 50
+        }
+        if (dataL === 0 && dataP === 1) {
+            P = 50
+            L = 250
+        }
     }
-    if (dataP === 0 && dataL === 1) {
-        P = 250
-        L = 50
-    }
-    if (dataL === 0 && dataP === 1) {
-        P = 50
-        L = 250
-    }
+    
     if (dataL === 1 && dataP === 1){
         radio.sendNumber(0)
         P = 0
         L = 0
+        krizovatka = true
     }
     
 })
